e321705975b8f7e69d1fb070d64c2f12285f9855
WoodsonJ <j.woodson@hotmail.fr>
2019-07-11 20:22:28 +0200

    Add behavior in order: - method displayAvailableMenu - method displaySelectedMenu

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,10 +1,17 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
+        System.out.println("Choix Menu \n" +
+                "1 - poulet \n" + 
+                "2 - boeuf \n" + 
+                "3 - végétarien \n" + 
+                "Que souhaitez-vous comme menu ?");
 
     }
     /**
@@ -12,6 +19,6 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-
+        System.out.println("Vous avez choisi le menu"+ " " + nbMenu);
     }
 }
--- test/com/ocr/anthony/OrderTest.java
+++ test/com/ocr/anthony/OrderTest.java
@@ -1,8 +1,8 @@
 package com.ocr.anthony;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
